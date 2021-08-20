03813588009a885ecd98574e765141a901465095
Anna <an.malch@gmail.com>
2021-08-20 13:17:42 +0200

    added __str__ methods for models

--- products/models.py
+++ products/models.py
@@ -7,6 +7,10 @@ class ProductCategory(models.Model):
     name = models.CharField(max_length=64, unique=True)
     description = models.TextField(blank=True, null=True) #данное поле может быть и пустым, тк blank=True, null=True
 
+    def __str__(self):
+        return self.name
+
+
 class Product(models.Model):
     name = models.CharField(max_length=256)
     image = models.ImageField(upload_to='products_images', blank=True)
@@ -14,3 +18,6 @@ class Product(models.Model):
     price = models.DecimalField(max_digits=8, decimal_places=2, default=0)
     quantity=models.PositiveIntegerField(default=0)
     category = models.ForeignKey(ProductCategory, on_delete=models.CASCADE) #каскадное удаление
+
+    def __str__(self):
+        return f'{self.name} | {self.category.name}'
