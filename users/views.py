bf07a235b41bd0f9e7afe7030098a8231fc88a42
Anna <an.malch@gmail.com>
2021-08-23 23:14:49 +0200

    added final steps

--- users/views.py
+++ users/views.py
@@ -35,3 +35,7 @@ def registration(request):
         form = UserRegistrationForm() #Get запрос
     context = {'title': 'GeekShop - Регистрация', 'form': form}
     return render(request, 'users/registration.html', context)
+
+def logout(request):
+    auth.logout(request)
+    return HttpResponseRedirect(reverse('index'))
