f81b5cf57ce4b640f0b84506d50b0060f5652ddf
Anna <an.malch@gmail.com>
2021-09-05 22:26:14 +0200

    created  controller for READ users

--- admins/views.py
+++ admins/views.py
@@ -1,6 +1,14 @@
 from django.shortcuts import render
 
+from users.models import User
+
 
 def index(request):
     context = {'title': 'GeekShop - Admin'}
     return render(request, 'admins/index.html', context)  # возвращается генерация шаблона
+
+
+# CRUD: Read
+def admin_users(request):
+    context = {'title': 'GeekShop - Пользователи', 'users': User.objects.all()}
+    return render(request, 'admins/admin-users.html', context)
