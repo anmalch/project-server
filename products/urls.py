56e15af31a5d743cff1abd6338b608cb38039dcc
Anna <an.malch@gmail.com>
2021-09-14 09:55:40 +0200

    rewrite products controller to CBV

--- products/urls.py
+++ products/urls.py
@@ -5,7 +5,7 @@ from products.views import ProductsListView
 app_name = 'products'
 
 urlpatterns = [
-    path('', ProductsListView.as_view(), name='product'),  # при переходе в каталог, срабатывает url-адрес: /products
-    path('<int:category_id>/', ProductsListView.as_view(), name='category'),
+    path('', ProductsListView.as_view(), name='index'),  # при переходе в каталог, срабатывает url-адрес: /products
+    path('<int:category_id>/', ProductsListView.as_view(), name='product'),
     path('page/<int:page>/', ProductsListView.as_view(), name='page'),  # логика отвечает за пагинацию
 ]
