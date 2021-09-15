f663f4ab1ee3d7d58db02f968442304934f1a89e
Anna <an.malch@gmail.com>
2021-09-15 17:21:58 +0200

    renamed register.html from registration.html

--- users/views.py
+++ users/views.py
@@ -18,7 +18,7 @@ class UserLoginView(CommonContextMixin, LoginView):
 class UserRegistrationView(CommonContextMixin, SuccessMessageMixin, CreateView):
     model = User
     form_class = UserRegistrationForm
-    template_name = 'users/registration.html'
+    template_name = 'users/register.html'
     success_url = reverse_lazy('users:login')
     success_message = 'Вы успешно зарегестрировались!'
     title = 'GeekShop - Регистрация'
@@ -42,48 +42,4 @@ class UserProfileView(CommonContextMixin, UpdateView):
 class UserLogoutView(LogoutView):
     pass
 
-# def login(request):
-#     if request.method == 'POST':
-#         form = UserLoginForm(data=request.POST)
-#         if form.is_valid():
-#             username = request.POST['username']
-#             password = request.POST['password']
-#             user = auth.authenticate(username=username, password=password)
-#             if user and user.is_active:
-#                 auth.login(request, user)
-#                 return HttpResponseRedirect(reverse('index'))
-#     else:
-#         form = UserLoginForm()
-#     context = {'title': 'GeekShop - Авторизация', 'form': form}
-#     return render(request, 'users/login.html', context)
 
-
-# def register(request):
-#     if request.method == 'POST':
-#         form = UserRegisterForm(data=request.POST)
-#         if form.is_valid():
-#             form.save()
-#             messages.success(request, 'Вы успешно зарегестрировались!')
-#             return HttpResponseRedirect(reverse('users:login'))
-#     else:
-#         form = UserRegisterForm()
-#     context = {'title': 'GeekShop - Регистрация', 'form': form}
-#     return render(request, 'users/register.html', context)
-
-
-# @login_required
-# def profile(request):
-#     user = request.user
-#     if request.method == 'POST':
-#         form = UserProfileForm(data=request.POST, files=request.FILES, instance=user)
-#         if form.is_valid():
-#             form.save()
-#             return HttpResponseRedirect(reverse('users:profile'))
-#     else:
-#         form = UserProfileForm(instance=user)
-#     context = {
-#         'title': 'GeekShop - Личный кабинет',
-#         'form': form,
-#         'baskets': Basket.objects.filter(user=user),
-#     }
-#     return render(request, 'users/profile.html', context)
